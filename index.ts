7027a3b46ad84fbb8703fc6ec0c9fbd5dc9b4d76
Kim, Hyeonseo <dodok8@gmail.com>
2025-05-13 23:44:25 +0900

    feat: implement song fetching and sorting functionality

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import { fetchWiki } from "./src/fetchWiki";
+import { parseSongList } from "./src/pareSongList";
+import { parseSong } from "./src/parseSong";
+
+import { write } from "bun";
+import * as semver from "semver";
+
+const songListHtml = await fetchWiki("曲目列表");
+const songList = parseSongList(songListHtml);
+console.log(songList.length);
+
+const allSongs = [];
+
+for (const song of songList) {
+  console.log(song);
+  const songHtml = await fetchWiki(song);
+  allSongs.push(parseSong(songHtml));
+}
+
+const sortedSongs = allSongs.sort((a, b) => {
+  if (!a.releaseVersion || !b.releaseVersion) return 0;
+  return semver.compare(a.releaseVersion, b.releaseVersion);
+});
+
+await write("data/songs.json", JSON.stringify(sortedSongs, null, 2));
+console.log("All songs data has been saved to songs.json");
