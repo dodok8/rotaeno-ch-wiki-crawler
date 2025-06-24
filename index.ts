2b975298211854f36a8874070337f18b9b42a12f
Kim, Hyeonseo <dodok8@gmail.com>
2025-06-24 18:45:14 +0900

    feat: add continue feature

--- index.ts
+++ index.ts
@@ -1,23 +1,31 @@
-import { downloadAvif } from "./src/downloadAvif";
+import type { Song } from "./src/@types";
 import { fetchWiki } from "./src/fetchWiki";
 import { parseSongList } from "./src/pareSongList";
 import { parseSong } from "./src/parseSong";
+import data from "./data/songs.json";
 
 import { write } from "bun";
 import * as semver from "semver";
 
 const songListHtml = await fetchWiki("曲目列表");
 const songList = parseSongList(songListHtml);
-// const songList = ["After Rain"];
 
 console.log(songList.length);
 
-const allSongs = [];
+const allSongs: Song[] = data;
 
 for (const song of songList) {
   console.log(song);
-  const songHtml = await fetchWiki(song);
-  allSongs.push(parseSong(songHtml));
+  if (
+    allSongs.findIndex((item) => {
+      return item.title_localized.default === song;
+    }) !== -1
+  ) {
+    console.log("continue");
+  } else {
+    const songHtml = await fetchWiki(song);
+    allSongs.push(parseSong(songHtml));
+  }
 }
 
 const sortedSongs = allSongs.sort((a, b) => {
@@ -25,5 +33,5 @@ const sortedSongs = allSongs.sort((a, b) => {
   return semver.compare(a.releaseVersion, b.releaseVersion);
 });
 
-await write("data/songs_after.json", JSON.stringify(sortedSongs, null, 2));
+await write("data/songs.json", JSON.stringify(sortedSongs, null, 2));
 console.log("All songs data has been saved to songs.json");
